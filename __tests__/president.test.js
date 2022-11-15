d9f35797b7f44b818545f82b3f8e79c7877bbeda
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-11-14 17:13:39 -0800

    4/4 pass

--- __tests__/president.test.js
+++ __tests__/president.test.js
@@ -8,14 +8,25 @@ describe('past pet route', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('/pokemon route returns list', async () => {
+  it('/prez route', async () => {
     const res = await request(app).get('/presidents');
     const expected = presidentList.map((president) => {
       return { id: president.id, name: president.name, party: president.party };
     });
     expect(res.body).toEqual(expected);
   });
-});
-afterAll(() => {
-  pool.end();
+  it('/president/:id detail route', async () => {
+    const res = await request(app).get('/presidents/1');
+    const expected = {
+      id: '1',
+      name: 'George',
+      party: 'republican',
+      number: 1,
+      country: 'US',
+    };
+    expect(res.body).toEqual(expected);
+  });
+  afterAll(() => {
+    pool.end();
+  });
 });
